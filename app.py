bba106cccbc877f0ec37dae459561520e3946806
PhongNVA <phongnva@PhongNVA.lan>
2023-05-22 20:12:31 +0700

    First Init

--- app.py
+++ app.py
@@ -6,20 +6,11 @@ app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
-def scale(payload):
-    """Scales Payload"""
-    
-    LOG.info(f"Scaling Payload: \n{payload}")
-    scaler = StandardScaler().fit(payload.astype(float))
-    scaled_adhoc_predict = scaler.transform(payload.astype(float))
-    return scaled_adhoc_predict
-
 @app.route("/")
 def home():
     html = f"<h3>Hello World, my project name PhongNVA</h3>"
     return html.format(format)
 
 if __name__ == "__main__":
-    clf = joblib.load("./model_data/boston_housing_prediction.joblib")
     app.run(host='0.0.0.0', port=80, debug=True)    
 
