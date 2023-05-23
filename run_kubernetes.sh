9f9655024e45140ab65550a225b8dff75039e702
PhongNVA <phongnva@PhongNVA-2.local>
2023-05-23 23:05:28 +0700

    First Init

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -14,10 +14,9 @@ kubectl create deploy my-running-app --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
-# kubectl get pods
 podname=$(kubectl get pods -o name)
 
 # Step 4:
 # Forward the container port to a host
 sleep 60
-kubectl port-forward $podname 0.0.0.0 8000:80
+kubectl port-forward $podname --address 0.0.0.0 8000:80 > /dev/null 2>&1 &
